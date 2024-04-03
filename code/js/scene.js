2a1e9b22eeb3a2da4d7eef1bfec00f2b0f597d87
D-Mathew <DMathewToh1999@gmail.com>
2024-04-03 15:00:01 -0400

    added terrain

--- code/js/scene.js
+++ code/js/scene.js
@@ -1,9 +1,9 @@
 import * as THREE from "three";
 import * as ASSETS from "/code/js/assets.js"
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
 
 export function populateScene(scene, updatables) {
     scene.renderer.setClearColor(ASSETS.SkyColors.day);
-    
     scene.camera.position.setZ(25);
     scene.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
@@ -15,6 +15,35 @@ export function populateScene(scene, updatables) {
     // TODO: Use a cubemap for the skybox
 
     // TODO: Add terrain
+    const terrainGeometry = new THREE.PlaneGeometry(300, 300, 60, 60); // Width, height, width segments, height segments
+    const positions = terrainGeometry.getAttribute('position')
+    for (let i = 0; i < positions.count; i++) {
+        const z = positions.getZ(i);
+        positions.setZ(i, z * 50); // Increase this multiplier
+    }
+    const textureLoader = new THREE.TextureLoader();
+    const terrainMaterial = new THREE.MeshStandardMaterial({
+        map: textureLoader.load('code/textures/ice_0001_1k_APRx3a/ice_0001_color_1k.jpg'),
+        normalMap: textureLoader.load('code/textures/ice_0001_1k_APRx3a/ice_0001_normal_opengl_1k.png'),
+        displacementMap: textureLoader.load('code/textures/ice_0001_1k_APRx3a/ice_0001_height_1k.png'),
+        aoMap: textureLoader.load('code/textures/ice_0001_1k_APRx3a/ice_0001_ao_1k.jpg'),
+        roughnessMap: textureLoader.load('code/textures/ice_0001_1k_APRx3a/ice_0001_roughness_1k.jpg'),
+        color: 0xFFFFFF
+    }); // Simple white material for snow
+    const terrain = new THREE.Mesh(terrainGeometry, terrainMaterial);
+    terrain.rotation.x = -Math.PI / 2; // Rotate the plane to lay it flat
+    scene.add(terrain);
+
+    // Lighting
+    const light = new THREE.DirectionalLight(0xffffff, 4);
+    light.position.set(1, 1, 1).normalize(); // Position the light to shine from above
+    scene.add(light);
+
+    // Camera position
+    scene.camera.position.set(100, 50, 50);
+    scene.camera.lookAt(scene.position);
+
+    // TODO: Add Roads
 
     // TODO: Add instanced trees
 
